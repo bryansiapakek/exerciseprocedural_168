472dfa279df4842837c44dffb481176dc5d65906
bryan erdanto <bryanputra455@gmail.com>
2023-03-27 15:12:58 +0700

    Add project files.

---
+++ exerciseprocedural_168/exerciseprocedural_168.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+double rerata(double a, double b)
+{
+	return (a + b) / 2;
+}
+string status(double rata) {
+	if (rata >= 70)
+		return " diterima";
+	else
+		return "gagal"
+}
+string status2(double rata, double nil) {
+	if (rata >= 70 or nil >= 80)
+		return "diterima";
+	else
+		return "gagal"
+}
+string nama;
+int j;
+
+void input() {
+	for (int i = 0; i < 20; i++) {
+		cout << "Masukkan Nama = ";
+	}
+	return 0;
+}
+int main()
+{
+	input();
+}
+
+
+
+
+
+
